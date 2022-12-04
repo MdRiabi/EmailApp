852df535faee58db0812e224aabb30618f8cd9e7
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-04 13:57:03 +0100

    build a custom validator and using it in signup component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,7 +10,8 @@ import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
 @NgModule({
   declarations: [
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    
   ],
   imports: [
     CommonModule,
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatchPassword } from '../validatros/match-password';
 
 
 @Component({
@@ -7,33 +8,38 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
+  constructor(private matchPassword: MatchPassword) { }
   authForm = new FormGroup({
     userName: new FormControl('',
-    [
+      [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(25),
+        Validators.pattern(/^[a-z0-9]+$/),
+      ]),
+    password: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
       Validators.maxLength(25),
-      Validators.pattern(/^[a-z0-9]+$/),
-]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(25),
 
-      ]),
-      passwordConfirmation: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(25),
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(25),
 
-      ]),
-});
+    ]),
+  },
+    { validators: [this.matchPassword.validate]});
 
 
-  constructor() { }
+  
 
   ngOnInit(): void {
   }
 
 }
+
+
