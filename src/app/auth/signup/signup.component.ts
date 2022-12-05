03f51451ef3bd77c007484c59cefde7921684993
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-12-05 19:16:23 +0100

    managing response error by using subscribe methods

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { AuthService } from '../auth.service';
 import { MatchPassword } from '../validatros/match-password';
 import { UniqueUsername } from '../validatros/unique-username';
@@ -40,19 +41,33 @@ export class SignupComponent implements OnInit {
     private matchPassword: MatchPassword,
     private uniqueUsername: UniqueUsername,
     private authServices: AuthService
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   onSubmit() {
     if (this.authForm.invalid) {
       return;
-    } 
-      return this.authServices
-        .signup(this.authForm.value)
-        .subscribe((response) => {
-          console.log(response);
-        });
-    
+    }
+    return this.authServices
+      .signup(this.authForm.value)
+      .subscribe({
+
+        next: response => {
+          console.log(response)
+        },
+        error: err => {
+          if (!err.status) {
+            this.authForm.setErrors({ noConnection: true });
+          }
+          else{
+            this.authForm.setErrors({ unknownError: true });
+
+          }
+        }
+
+
+      });
+
     /* console.log(this.authForm.value); */
   }
 }
